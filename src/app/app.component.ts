81283020b693c8d247b05fb4f4348305912a5f8e
guilhermemm-dev <guilhermemm.dev@gmail.com>
2022-07-30 10:49:53 +0100

    contador concluido

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-counter';
+
+  countNumber = 0;
+
+  resetBtn() {
+    this.countNumber = 0;
+  }
+
+  increaseBtn() {
+    this.countNumber = this.countNumber + 1;
+  }
+
+  decreaseBtn() {
+    this.countNumber = this.countNumber - 1;
+    if (this.countNumber < 0) {
+      this.countNumber = 0;
+    }
+  }
 }
